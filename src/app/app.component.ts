9f771049b600d067c29e3b46b370d96f4acb84d2
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-01-24 23:58:03 -0300

    Diretivas, @Input, @Output e EventEmitter, ngStyle, ngClass.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,46 +11,11 @@ interface Funcionario {
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  ultimoId = 0;
-  nome = 'Albert';
-  idade = 43;
-  adicionado = false;
+
   funcionarios: Funcionario[] = [];
 
-  getIdade() {
-    return this.idade;
+  aoAdicionar(funcionario: Funcionario) {
+    this.funcionarios.push(funcionario);
   }
-
-  adicionar() {
-    console.log(`Adicionando ${this.nome}`);
-    this.adicionado = true;
-
-    this.funcionarios.push({
-      id: ++this.ultimoId,
-      nome: this.nome
-    });
-  }
-
-// adicionar() {
-//   console.log(`Adicionando ${this.nome}`);
-//   this.adicionado = true;
-
-//   const numero = Math.round(Math.random() * 100);
-//   this.nome = 'Albert ' + numero;
-
-// }
-
-// adicionar(nome: string) {
-//   this.nome = nome;
-// }
-
-// alterarNome(event: any) {
-//   // console.log(event);
-//   this.nome = event.target.value;
-// }
-
-// alterarNome(event: any) {
-//   console.log(event.target.value);
-// }
-
+  
 }
--- src/app/funcionario-card/funcionario-card.component.ts
+++ src/app/funcionario-card/funcionario-card.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 
 @Component({
   selector: 'app-funcionario-card',
   templateUrl: './funcionario-card.component.html',
-  styleUrl: './funcionario-card.component.css'
+  // styleUrl: './funcionario-card.component.css'
+  styles: [`
+    .card-block {
+      text-transform: uppercase;
+      color: blue;
+    }
+  `]
 })
 export class FuncionarioCardComponent {
 
-  funcionario: { id: number, nome: string } = { id: 1, nome: 'Albert'};
+  //No input, podemos passar um alias para o atributo que será recebido pelo componente filho (funcionario-card).
+  @Input('func') funcionario: any;
+
+  getEstilosCartao() {
+    return {
+      'border-width.px': this.funcionario.id,
+      backgroundColor: this.funcionario.id % 2 === 0 ? 'lightblue' : 'lightgreen'
+    };
+  }
+  
+  isAdmin() {
+    return this.funcionario.nome.startsWith('T');
+  }
+
+  // getClassesCss() {
+  //   return ['badge', 'badge-default'];
+  // }
 }
