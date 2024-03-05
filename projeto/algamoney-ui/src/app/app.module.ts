709324c213f34cc0b9c50f1153685b5b8594efde
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-03-04 21:26:49 -0300

    Tabela em Construção

---
+++ projeto/algamoney-ui/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TabViewModule } from 'primeng/tabview';
+import { AppComponent } from './app.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+
+    ButtonModule,
+    InputTextModule,
+    TabViewModule,
+    TableModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
