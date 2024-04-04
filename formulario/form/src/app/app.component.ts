3a0c718998977b79a3757219ce81c86de3d733c3
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-03 22:18:56 -0300

    validações

--- formulario/form/src/app/app.component.ts
+++ formulario/form/src/app/app.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+class Cliente {
+  nome: string;
+  email: string;
+  profissao = '';
+
+  public constructor(nome = '', email = '') {
+    this.nome = nome;
+    this.email = email;
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'form';
+
+  cliente = new Cliente();
+
+  profissoes = ['Engenheiro', 'Professor', 'Químico', 'Programador', 'Outra'];
 
   salvar(form: NgForm) {
+    // this.cliente.nome = form.value.nome;
+    // this.cliente.email = form.value.email;
+    // this.cliente.profissao = form.value.profissao;
+
     console.log(form);
+    // console.log(this.cliente);
   }
 }
