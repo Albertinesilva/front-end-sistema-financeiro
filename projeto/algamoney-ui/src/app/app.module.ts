c245cb8c7a3a6469ca967ad37f4b4a08ae4ad340
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-21 01:22:07 -0300

    Pesquisa Pessoas

--- projeto/algamoney-ui/src/app/app.module.ts
+++ projeto/algamoney-ui/src/app/app.module.ts
@@ -1,28 +1,39 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { TabViewModule } from 'primeng/tabview';
-import { AppComponent } from './app.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
+import { CalendarModule } from 'primeng/calendar';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
+import { AppComponent } from './app.component';
 import { LancamentoPesquisaComponent } from './components/lancamento-pesquisa/lancamento-pesquisa.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LancamentoCadastroComponent } from './components/lancamento-cadastro/lancamento-cadastro.component';
+import { PessoasPesquisaComponent } from './components/pessoas-pesquisa/pessoas-pesquisa.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LancamentoPesquisaComponent,
-    NavbarComponent
+    NavbarComponent,
+    LancamentoCadastroComponent,
+    PessoasPesquisaComponent
   ],
   imports: [
     BrowserModule,
-
+    BrowserAnimationsModule,
     ButtonModule,
     InputTextModule,
     TabViewModule,
     TableModule,
-    TooltipModule
+    TooltipModule,
+    InputTextareaModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
