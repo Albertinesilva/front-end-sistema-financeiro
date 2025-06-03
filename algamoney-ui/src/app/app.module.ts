631c802c22d5ebb49eb6e861635a6211ba886897
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-06-03 14:56:42 -0300

    CoreModule

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -3,21 +3,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './core/navbar/navbar.component';
 import { LancamentosModule } from './features/lancamentos/lancamentos.module';
 import { PessoasModule } from './features/pessoas/pessoas.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
 
     LancamentosModule,
-    PessoasModule
+    PessoasModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
